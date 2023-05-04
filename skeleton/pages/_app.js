26766a90d59564bf22932ee0293337f09dc68bae
Thi Thuy Oanh Nguyen <thuyoanh199930@gmail.com>
2023-05-04 08:36:45 +0000

    add form and its url

--- skeleton/pages/_app.js
+++ skeleton/pages/_app.js
@@ -4,3 +4,5 @@ import '../styles/tailwind.css'
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
+//By default, Next.js will automatically import the _app.js file for every page in your application.
+
