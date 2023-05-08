affa79598f4ee0510aad5389a53518c24960cb5b
Pedro Lorandi <hello@pedrolorandi.com>
2023-05-08 14:57:41 -0400

    Add comment to seeds.js file

--- skeleton/prisma/seed.js
+++ skeleton/prisma/seed.js
@@ -8,30 +8,35 @@ const transactions = require("./transactions.json");
 const prisma = new PrismaClient();
 
 async function main() {
+  // Users
   for (const user of users) {
     await prisma.user.create({
       data: user,
     });
   }
 
+  // Accounts
   for (const account of accounts) {
     await prisma.account.create({
       data: account,
     });
   }
 
+  // Categories
   for (const category of categories) {
     await prisma.category.create({
       data: category,
     });
   }
 
+  // Sources
   for (const source of sources) {
     await prisma.source.create({
       data: source,
     });
   }
 
+  // Transactions
   for (const transaction of transactions) {
     await prisma.transaction.create({
       data: transaction,
