9fdc13a700511cbb10f7d3996b92aec49225bd05
Pedro Lorandi <hello@pedrolorandi.com>
2023-05-08 14:31:12 -0400

    Fix seeds.js file

--- skeleton/prisma/seed.js
+++ skeleton/prisma/seed.js
@@ -1,8 +1,3 @@
-// import { users } from "../prisma/users.js";
-// import { accounts } from "../prisma/accounts.js";
-// import { sources } from "../prisma/sources.js";
-// import { categories } from "../prisma/categories.js";
-// import { transactions } from "../prisma/transactions.js";
 const { PrismaClient } = require("@prisma/client");
 const users = require("./users.json");
 const accounts = require("./accounts.json");
@@ -13,41 +8,6 @@ const transactions = require("./transactions.json");
 const prisma = new PrismaClient();
 
 async function main() {
-  // npx prisma db push --force-reset && npx prisma db push && npx prisma db seed
-
-  // // Users run first
-  // await Promise.all(
-  //   users.map(async (user) => {
-  //     await prisma.user.create({ data: user });
-  //   })
-  // );
-
-  // // Accounts
-  // const accountPromises = accounts.map(async (account) => {
-  //   return prisma.account.create({ data: account });
-  // });
-
-  // // Sources
-  // const sourcePromises = sources.map(async (source) => {
-  //   return prisma.source.create({ data: source });
-  // });
-
-  // // Categories
-  // const categoryPromises = categories.map(async (category) => {
-  //   return prisma.category.create({ data: category });
-  // });
-
-  // // Wait for all the promises to complete before continuing
-  // await Promise.all([
-  //   ...accountPromises,
-  //   ...sourcePromises,
-  //   ...categoryPromises,
-  // ]);
-
-  // const transactionPromises = transactions.map(async (transaction) => {
-  //   return prisma.transaction.create({ data: transaction });
-  // });
-
   for (const user of users) {
     await prisma.user.create({
       data: user,
