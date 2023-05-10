8cfe8f56c44665256220e97f75cff909730e2fe1
Jaewon Bae <jaey.pro@gmail.com>
2023-05-09 21:53:45 -0400

    create budget route

---
+++ skeleton/pages/budgets/index.js
@@ -0,0 +1,13 @@
+import { PrismaClient } from "@prisma/client";
+
+import { getDateByMonthYear, getTransactions } from "../../helpers/selectors";
+import axios from "axios";
+import { useState } from "react";
+
+export default function Budgets() {
+  return (
+    <div>
+      <div>hello</div>
+    </div>
+  );
+}
