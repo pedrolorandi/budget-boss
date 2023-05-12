6aba0f4da7c83d74bd1ac1c9bc35b1c40180bd18
Jaewon Bae <jaey.pro@gmail.com>
2023-05-11 20:32:22 -0400

    remove commented out code

--- skeleton/components/ui/BudgetCategoriesList.js
+++ skeleton/components/ui/BudgetCategoriesList.js
@@ -4,63 +4,6 @@ import { useState } from "react";
 import BarChart from "./BudgetProgressBar";
 
 export default function BudgetCategoriesList(props) {
-  // const [chartData, setChartData] = useState({
-  //   labels: props.budgetAmounts.map((data) => data.name),
-  //   datasets: [
-  //     {
-  //       labels: props.budgetAmounts.map((data) => data.name),
-  //       data: props.budgetAmounts.map((data) => data.budgetPercentage),
-  //       backgroundColor: ["green", "blue"],
-  //       borderWidth: 1,
-  //       borderSkipped: false,
-  //       borderRadius: 5,
-  //       barPercentage: 0.2,
-  //       categoryPercentage: 0.9,
-  //     },
-  //   ],
-  // });
-
-  // const [chartOptions, setChartOptions] = useState({
-  //   indexAxis: "y",
-  //   scales: {
-  //     x: {
-  //       suggestedMin: 0,
-  //       suggestedMax: 100,
-  //       grid: { display: false },
-  //       ticks: { display: false },
-  //     },
-  //     y: {
-  //       beginAtZero: true,
-  //       grid: { display: false },
-  //       ticks: { display: false },
-  //     },
-  //   },
-  //   plugins: {
-  //     legend: {
-  //       display: false,
-  //     },
-  //   },
-  // });
-
-  const transactions = props.transactionsByCategory.map((element, i) => {
-    return (
-      <div key={i}>
-        {element.categoryId} {element.name} {element._sum.amountDecimal / 100}
-      </div>
-    );
-  });
-
-  const budgets = props.budgets.map((element, i) => {
-    return (
-      <div key={i}>
-        {element.Category.id} {element.Category.name}{" "}
-        {element.amountDecimal / 100}
-      </div>
-    );
-  });
-
-  console.log(props.budgetAmounts);
-
   const progressCharts = props.budgetAmounts.map((element, i) => {
     return (
       <BarChart
