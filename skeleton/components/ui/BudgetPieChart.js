33544a87fa1da2f77d448468e8629210d1e21a8a
Jaewon Bae <jaey.pro@gmail.com>
2023-05-17 16:52:47 -0400

    remove console.log from pie chart

--- skeleton/components/ui/BudgetPieChart.js
+++ skeleton/components/ui/BudgetPieChart.js
@@ -3,7 +3,6 @@ import { Doughnut } from "react-chartjs-2";
 import { Chart as ChartJS } from "chart.js/auto";
 
 export default function BudgetPieChart(props) {
-  console.log(props);
   return (
     <div className="w-1/4 container relative">
       <div className="z-10 flex justify-center text-4xl absolute m-auto right-1 left-1 bottom-1/3 top-1/2">
