dc83098c4f6a72c64679161420ada2f46bb317ba
Jaewon Bae <jaey.pro@gmail.com>
2023-05-18 00:42:30 -0400

    refactor bar chart to show budget totals

--- skeleton/components/ui/CategoryBarChart.js
+++ skeleton/components/ui/CategoryBarChart.js
@@ -5,7 +5,7 @@ import { Chart as ChartJS } from "chart.js/auto";
 export default function CategoryBarChart({ chartData, options }) {
   return (
     <div className="flex w-full bg-[#F2F7FC] p-5 rounded-lg mt-2">
-      <Chart data={chartData} options={options} />
+      <Chart data={chartData} />
     </div>
   );
 }
