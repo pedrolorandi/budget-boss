9621bb1b82b2cd338bd36168691ccdeb2b2e73c5
Jaewon Bae <jaey.pro@gmail.com>
2023-05-23 17:52:18 -0400

    fix title warning

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,17 @@
 import Layout from "@/components/layout/Layout";
 import "@/styles/globals.css";
 import "@/styles/tailwind.css";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Head>
+        <title>BudgetBoss</title>
+      </Head>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   );
 }
